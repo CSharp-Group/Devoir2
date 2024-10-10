31e66a90eb35ac7df70215860aa9bdbeed4ee695
Jean-Michel <jeanmialbert10@hotmail.com>
2024-10-10 09:06:31 -0300

    Fixes to branch

--- FicheAlimentEnfantForm.cs
+++ FicheAlimentEnfantForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,10 @@ namespace FicheAliments
     public partial class FicheAlimentEnfantForm : Form
     {
         public static int numero = 1;
+        #region Variables
+        private bool enregistrementBool = false;
+        private bool modificationBool = false;
+        #endregion
 
         public FicheAlimentEnfantForm()
         {
@@ -29,6 +34,88 @@ namespace FicheAliments
             {
                 throw new IndexOutOfRangeException("Erreur");
             }
+        }
+
+        #region Propriétés
+        public bool Enregistrement
+        {
+            get
+            {
+                return enregistrementBool;
+            }
+            set
+            {
+                enregistrementBool = value;
+            }
+        }
+
+        public bool Modification
+        {
+            get
+            {
+                return modificationBool;
+            }
+            set
+            {
+                modificationBool = value;
+            }
+        }
+        #endregion
+
+        public void Enregistrer(FicheAlimentEnfantForm alimentEnfant)
+        {
+            if (alimentEnfant.infoRichTextBox.Modified || Modification)
+            {
+                if (!Enregistrement)
+                {
+                    EnregistrerSous(alimentEnfant);
+                }
+                else
+                {
+                    RichTextBox ortf = new RichTextBox
+                    {
+                        Rtf = alimentEnfant.infoRichTextBox.Rtf
+                    };
+
+                    ortf.SelectionStart = 0;
+                    ortf.SelectionLength = 0;
+                    ortf.SelectedText = alimentEnfant.nomTextBox.Text + Environment.NewLine;
+
+                    ortf.SaveFile(alimentEnfant.Text);
+
+                    Modification = false;
+                    alimentEnfant.infoRichTextBox.Modified = false;
+                }
+            }
+        }
+
+        public void EnregistrerSous(FicheAlimentEnfantForm alimentEnfant)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Fichiers RTF|*.rtf";
+                sfd.Title = "Enregistrer sous";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (Path.GetExtension(sfd.FileName).ToLower() != ".rtf")
+                    {
+                        MessageBox.Show("Veuillez choisir un fichier avec l'extension .rtf.");
+                        return;
+                    }
+
+                    alimentEnfant.infoRichTextBox.SaveFile(sfd.FileName);
+                    alimentEnfant.Text = sfd.FileName; // Met à jour le titre du formulaire
+
+                    Enregistrement = true;
+                    Modification = false;
+                    alimentEnfant.infoRichTextBox.Modified = false;
+                }
+            }
+        }
+        private void clientTextBox_TextChanged(object sender, EventArgs e)
+        {
+            Modification = true;
         }
     }
 }
